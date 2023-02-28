5e3887900f04badfe1a1093ff2f7bc509a2bf41e
KM <siukwongman@gmail.com>
2023-02-28 22:27:24 +0800

    Update: MVC for member page

--- frontend/public/js/member/member.main.js
+++ frontend/public/js/member/member.main.js
@@ -1,10 +1,3 @@
 import userSigninStatus from "../signin/signin.userSigninStatus.js";
 
-try{
-    await Promise.all([
-        userSigninStatus()
-    ]);
-}
-catch(err){
-    console.log(err);
-};
+userSigninStatus()
